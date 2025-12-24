a85a12ad1768cb71ce4e53118f032ca26c12b302
Gabriel Marin <marinjhow@gmail.com>
2025-12-24 11:45:27 -0300

    feat: Add JpaRepository to FunctionaryModel and SectorModel

---
+++ src/main/java/dev/java10x/employee_management/Functionarys/FunctionaryRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.employee_management.Functionarys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FunctionaryRepository extends JpaRepository<FunctionaryModel, Long> {
+}
---
+++ src/main/java/dev/java10x/employee_management/Sectors/SectorRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.employee_management.Sectors;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectorRepository extends JpaRepository<SectorModel, Long> {
+}
