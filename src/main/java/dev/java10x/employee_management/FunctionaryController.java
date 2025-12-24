b873e6150f29dc65bb4bb8358b9be5774478118f
Gabriel Marin <marinjhow@gmail.com>
2025-12-24 09:11:36 -0300

    feat: file name change and JPA dependency added

--- src/main/java/dev/java10x/employee_management/FunctionaryController.java
+++ src/main/java/dev/java10x/employee_management/FunctionaryController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class FunctionaryController {
     @GetMapping("/registration")
     public String registration() {
         return "Test Test Test";
---
+++ src/main/java/dev/java10x/employee_management/FunctionaryModel.java
@@ -0,0 +1,88 @@
+package dev.java10x.employee_management;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_functionary")
+public class FunctionaryModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+    private String position;
+    private LocalDate dateAdmission;
+    private boolean active;
+
+    public FunctionaryModel() {
+    }
+
+    public FunctionaryModel(String name, String cpf, String email, String position, LocalDate dateAdmission, boolean active) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.position = position;
+        this.dateAdmission = dateAdmission;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public LocalDate getDateAdmission() {
+        return dateAdmission;
+    }
+
+    public void setDateAdmission(LocalDate dateAdmission) {
+        this.dateAdmission = dateAdmission;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
