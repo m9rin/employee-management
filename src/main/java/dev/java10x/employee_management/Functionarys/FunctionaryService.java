7db1b1d99896b1eb83833285b7bd1a6d76e6ab8b
Gabriel Marin <marinjhow@gmail.com>
2025-12-27 16:52:33 -0300

    feat: adds route findById

--- src/main/java/dev/java10x/employee_management/Functionarys/FunctionaryService.java
+++ src/main/java/dev/java10x/employee_management/Functionarys/FunctionaryService.java
@@ -3,6 +3,7 @@ package dev.java10x.employee_management.Functionarys;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FunctionaryService {
@@ -15,5 +16,11 @@ public class FunctionaryService {
 
     public List<FunctionaryModel> listFunctionary() {
         return functionaryRepository.findAll();
+    }
+
+    public FunctionaryModel functionaryById(long id) {
+        Optional<FunctionaryModel> functionaryById = functionaryRepository.findById(id);
+        return functionaryById.orElse(null);
+
     }
 }
--- src/main/java/dev/java10x/employee_management/Sectors/SectorService.java
+++ src/main/java/dev/java10x/employee_management/Sectors/SectorService.java
@@ -3,6 +3,7 @@ package dev.java10x.employee_management.Sectors;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SectorService {
@@ -15,5 +16,10 @@ public class SectorService {
 
     public List<SectorModel> listSector() {
         return sectorRepository.findAll();
+    }
+
+    public SectorModel sectorById(long id) {
+        Optional<SectorModel> sectorById = sectorRepository.findById(id);
+        return sectorById.orElse(null);
     }
 }
