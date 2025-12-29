e8ad5c1bc71a5f7b2d2fe85b6ad99111bbdcb04e
Gabriel Marin <marinjhow@gmail.com>
2025-12-29 17:45:39 -0300

    refactor: refactors the endpoints and adds apiresponse documentation

--- src/main/java/dev/java10x/employee_management/Controllers/FunctionaryController.java
+++ src/main/java/dev/java10x/employee_management/Controllers/FunctionaryController.java
@@ -1,6 +1,10 @@
-package dev.java10x.employee_management.Functionarys;
+package dev.java10x.employee_management.Controllers;
 
+import dev.java10x.employee_management.DTOs.FunctionaryDTO;
+import dev.java10x.employee_management.Services.FunctionaryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +24,50 @@ public class FunctionaryController {
 
     @GetMapping("/list")
     @Operation(summary = "List all employees", description = "Create a list with all registered employees")
+    @ApiResponse(responseCode = "200", description = "Return a list of all registered employees in JSON format")
     public ResponseEntity<List<FunctionaryDTO>> list() {
         List<FunctionaryDTO> functionarys = functionaryService.list();
         return ResponseEntity.ok(functionarys);
     }
 
     @GetMapping("/list/{id}")
+    @Operation(summary = "Lists the employee by ID", description = "Lists the employee whose ID was passed as a parameter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return employee whose ID was passed as a parameter"),
+            @ApiResponse(responseCode = "404", description = "Return not found if the ID does not exists")
+    })
     public ResponseEntity<FunctionaryDTO> listById(@PathVariable long id) {
         FunctionaryDTO functionary = functionaryService.listById(id);
         return ResponseEntity.ok(functionary);
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Register an employee", description = "Register an employee and assign it to a sector")
+    @ApiResponse(responseCode = "201", description = "Returns the registered employee with all their data in JSON format")
     public ResponseEntity<FunctionaryDTO> register(@RequestBody FunctionaryDTO functionaryDTO) {
         FunctionaryDTO saved = functionaryService.register(functionaryDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete by ID", description = "Deleted the employee whose ID was passed as a parameter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "No return in the body"),
+            @ApiResponse(responseCode = "404", description = "Return not found if the ID does not exists")
+    })
     public ResponseEntity<Void> delete(@PathVariable long id) {
             functionaryService.delete(id);
             return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/update/{id}")
+    @Operation(summary = "Update fields", description = "Updates permitted fields according to company needs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated sucessfully"),
+            @ApiResponse(responseCode = "400", description = "Error regarding the type of data passed or data is not allowed for partial update"),
+            @ApiResponse(responseCode = "404", description = "ID not found"),
+            @ApiResponse(responseCode = "500", description = "Attempt to rewrite unique data")
+    })
     public ResponseEntity<FunctionaryDTO> update(@PathVariable Long id, @RequestBody Map<String, Object> fields) {
         FunctionaryDTO updated = functionaryService.updated(id, fields);
         return ResponseEntity.ok(updated);
--- src/main/java/dev/java10x/employee_management/Controllers/SectorController.java
+++ src/main/java/dev/java10x/employee_management/Controllers/SectorController.java
@@ -1,5 +1,10 @@
-package dev.java10x.employee_management.Sectors;
+package dev.java10x.employee_management.Controllers;
 
+import dev.java10x.employee_management.DTOs.SectorDTO;
+import dev.java10x.employee_management.Services.SectorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,30 +23,51 @@ public class SectorController {
     }
 
     @GetMapping("/list")
+    @Operation(summary = "List all Sectors", description = "Create a list with all registered sectors")
+    @ApiResponse(responseCode = "200", description = "Return a list of all registered sectors in JSON format")
     public ResponseEntity<List<SectorDTO>> list() {
         List<SectorDTO> sectors = sectorService.list();
         return ResponseEntity.ok(sectors);
     }
 
     @GetMapping("/list/{id}")
+    @Operation(summary = "Lists the sector by ID", description = "Lists the sector whose ID was passed as a parameter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return sector whose ID was passed as a parameter"),
+            @ApiResponse(responseCode = "404", description = "Return not found if the ID does not exists")
+    })
     public ResponseEntity<SectorDTO> listById(@PathVariable long id) {
         SectorDTO sector = sectorService.listById(id);
         return ResponseEntity.ok(sector);
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Create an sector", description = "Create an sector")
+    @ApiResponse(responseCode = "201", description = "Returns the sector created with all their data in JSON format")
     public ResponseEntity<SectorDTO> register(@RequestBody SectorDTO sectorDTO) {
         SectorDTO saved = sectorService.register(sectorDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete by ID", description = "Deleted the sector whose ID was passed as a parameter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "No return in the body"),
+            @ApiResponse(responseCode = "404", description = "Return not found if the ID does not exists")
+    })
     public ResponseEntity<Void> delete(@PathVariable long id) {
        sectorService.delete(id);
        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/update/{id}")
+    @Operation(summary = "Update fields", description = "Updates permitted fields according to company needs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated sucessfully"),
+            @ApiResponse(responseCode = "400", description = "Error regarding the type of data passed or data is not allowed for partial update"),
+            @ApiResponse(responseCode = "404", description = "ID not found"),
+            @ApiResponse(responseCode = "500", description = "Attempt to pass data that does not exist")
+    })
     public ResponseEntity<String> sectorUpdate(@PathVariable Long id, @RequestBody Map<String, Object> fields) {
         if (sectorService.listById(id) != null) {
             sectorService.update(id, fields);
